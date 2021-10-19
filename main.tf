6119251dda368fd3bd15edec0aa8d5d768e92739
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2021-10-19 14:01:40 +0300

    commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    fastly = {
+      source  = "fastly/fastly"
+      version = "~> 0.30"
+    }
+  }
+}
+
+resource "fastly_service_v1" "test_service" { 
+ name = "An Example Service"
+
+  domain {
+    name = "jopapopa.global.ssl.fastly.net"
+  }
+
+  backend {
+    address = "httpbin.org"
+    name     = "My Test Backend"
+  }
+
+  force_destroy = true
+}
+
+output "active" {
+  value = fastly_service_v1.test_service.active_version
+}
